7b74d0c5ec376a08c97de56682aaca8b2b2dc523
Diyako-Qadri <qadri.diyako@gmail.com>
2024-03-14 09:44:37 +0100

    fixed the url queries, deleted css, fixed instruction text

--- server.js
+++ server.js
@@ -3,41 +3,52 @@ const url = require('url');
 const fs = require('fs');
 const PORT = 3030;
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, 'the response is OK');
+http.createServer((req, res) => {
+  res.writeHead(200, 'OK', {'Content-Type': 'text/html'});
+  let parsedUrl = url.parse(req.url, true);
+  let pathName = parsedUrl.pathname;
 
-    if (req.url === '/') {
+  if (pathName === '/') {
+    if (parsedUrl.query.genres) {
+      if ( parsedUrl.query.genres === "action") {
+        fs.readFile(`./routes/action.html`, (err, data) => {
+          if (err) throw err;
+          res.end(data);
+        });
+      } else if (parsedUrl.query.genres === "romantic") {
+        fs.readFile(`./routes/romantic.html`, (err, data) => {
+          if (err) throw err;
+          res.end(data);
+        });
+      } else if ( parsedUrl.query.genres === "drama") {
+        fs.readFile(`./routes/drama.html`, (err, data) => {
+          if (err) throw err;
+          res.end(data);
+        });
+      } else if ( parsedUrl.query.genres === "comedy") {
+        fs.readFile(`./routes/comedy.html`, (err, data) => {
+          if (err) throw err;
+          res.end(data);
+        });
+      } else {
+        res.write('Not Found');
+        res.end();
+      }
+    } else {
       fs.readFile(`./index.html`, (err, data) => {
-        res.end(data);
-      });
-    } else if (req.url.includes(`/action`)) {
-      fs.readFile(`./routes/action.html`, (err, data) => {
-        res.end(data);
-      });
-    } else if (req.url.includes(`/romantic`)) {
-      fs.readFile(`./routes/romantic.html`, (err, data) => {
-        res.end(data);
-      });
-    } else if (req.url.includes(`/drama`)) {
-      fs.readFile(`./routes/drama.html`, (err, data) => {
-        res.end(data);
-      });
-    } else if (req.url.includes(`/comedy`)) {
-      fs.readFile(`./routes/comedy.html`, (err, data) => {
-        res.end(data);
-      });
-    } else if (req.url.endsWith('.css')) {
-      fs.readFile('./style/main.css', (err, data) => {
+        if (err) throw err;
         res.end(data);
       });
     }
+  } else {
+    res.write('Not Found');
+    res.end();
+  }
+}).listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
 
-    if (!req.url) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not Found');
-    }
-  })
-  .listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-  });
+
+
+
+
